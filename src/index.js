564a5e5e2eeb993890a262488e602d5ebffc49db
Garmhausen <robertgarmhausen@gmail.com>
2017-08-30 00:20:53 -0400

    complete about styling and begin projects

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Scroll from 'react-scroll';
 import './index.css';
 
 // react-scroll: https://github.com/fisshy/react-scroll
-
 var Link = Scroll.Link;
 var Element = Scroll.Element;
 var Events = Scroll.Events;
@@ -18,13 +17,9 @@ class Section extends React.Component {
   }
 
   componentDidMount() {
-    Events.scrollEvent.register('begin', function() {
-      // console.log('begin', arguments);
-    });
+    Events.scrollEvent.register('begin');
 
-    Events.scrollEvent.register('end', function() {
-      // console.log('end', arguments);
-    });
+    Events.scrollEvent.register('end');
 
     scrollSpy.update();
   }
@@ -35,68 +30,112 @@ class Section extends React.Component {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
   }
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="nav-internal-wrapper">
-            <div
-              className="nav-links-wrapper"
-            >
-              <ul className="nav navbar-nav">
-                <li>
-                  <span className="navbar-brand nav-title">Robert Garmhausen</span>
-                </li>
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="about"
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
+            <div className="nav-links-wrapper">
+              <div className="nav">
+                <a
+                  className="navbar-brand nav-title"
+                  href="http://www.robertgarmhausen.com"
+                >
+                  Robert Garmhausen
+                </a>
+                <Link
+                  activeClass="active"
+                  className="about navlink"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  About
+                </Link>
+                <Link
+                  activeClass="active"
+                  className="projects navlink"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Projects
+                </Link>
+                <Link
+                  activeClass="active"
+                  className="contact navlink"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                >
+                  Contact
+                </Link>
+                <span className="nav-icons">
+                  <a
+                    href="https://www.linkedin.com/in/robertgarmhausen/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="projects"
-                    to="projects"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
+                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                  </a>
+                  <a
+                    href="https://github.com/Garmhausen"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
-                    Projects
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    activeClass="active"
-                    className="contact"
-                    to="contact"
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                  >
-                    Contact
-                  </Link>
-                </li>
-              </ul>
+                    <i className="fa fa-github-square" aria-hidden="true" />
+                  </a>
+                </span>
+              </div>
             </div>
           </div>
         </nav>
 
         <Element name="about" className="element about-section">
-          <div>
-            Hi! I'm Robert, a full-stack web developer, aspiring designer and
-            lover of algorithms.
+          <div className="about-wrapper">
+            <div className="about-text">
+              <div className="about-title">Hello</div>
+              Hi! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              vehicula lorem tellus, quis imperdiet nisl pellentesque eget.
+              Fusce ac viverra enim, ut hendrerit risus. Aenean ac dolor turpis.
+              Phasellus viverra aliquet sagittis. Nulla ac sagittis arcu, at
+              viverra velit. Aliquam nisi ipsum, placerat eu sagittis vitae,
+              elementum id nisl. Ut elementum, nisi ut facilisis porttitor, enim
+              nisi efficitur leo, ut fermentum velit arcu non nibh. In hac
+              habitasse platea dictumst. Sed venenatis commodo maximus. Nullam
+              consequat venenatis ipsum ac tincidunt. Nunc augue lacus, ornare
+              id odio sed, ultrices hendrerit tellus.
+            </div>
           </div>
+          <div className="skills-grid">Huge grid of skills will go here.</div>
         </Element>
 
         <Element name="projects" className="element projects-section">
-          Projects
+          <div className="projects-wrapper">
+            <div className="projects-card">
+              <div className="projects-card-left mysteryword-left">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  vehicula lorem tellus, quis imperdiet nisl pellentesque eget.
+                  Fusce ac viverra enim, ut hendrerit risus. Aenean ac dolor
+                  turpis. Phasellus viverra aliquet sagittis.
+                </p>
+              </div>
+              <div className="projects-card-right mysteryword-right">
+                  <a className="project-link" href="liveProject">Live
+                  </a>
+                  <a className="project-link" href="codeProject">
+                    Code
+                  </a>
+                </div>
+            </div>
+            <div className="projects-card">Card 2</div>
+            <div className="projects-card">Card 3</div>
+          </div>
         </Element>
 
         <Element name="contact" className="element contact-section">
